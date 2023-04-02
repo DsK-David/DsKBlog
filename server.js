7f62a13181346253ed8b1968812f1317a1a27a9c
DsK-David <fbi.gov2015@gmail.com>
2023-04-02 21:44:11 -0100

    add file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 const port = 3000
-const server = require('http').createServer(app)
+const server = require('https').createServer(app)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
